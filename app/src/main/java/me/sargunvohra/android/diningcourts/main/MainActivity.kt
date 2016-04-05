6f78da2dbbd206fd6d1e87009e290f8e92192f3b
Sargun Vohra <sargunster@gmail.com>
2016-04-05 14:23:41 -0400

    fab toolbar

--- app/src/main/java/me/sargunvohra/android/diningcourts/main/MainActivity.kt
+++ app/src/main/java/me/sargunvohra/android/diningcourts/main/MainActivity.kt
@@ -1,10 +1,14 @@
 package me.sargunvohra.android.diningcourts.main
 
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import kotlinx.android.synthetic.main.activity_main.*
 import me.sargunvohra.android.diningcourts.R
 import me.sargunvohra.android.diningcourts.base.BaseActivity
 import me.sargunvohra.android.diningcourts.config.dagger.MainModule
+import org.jetbrains.anko.info
+import org.jetbrains.anko.onClick
+import org.jetbrains.anko.support.v4.onPageChangeListener
 import java.util.*
 
 class MainActivity : BaseActivity() {
@@ -15,8 +19,27 @@ class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // setup action bar
         setSupportActionBar(toolbar)
+
+        // setup view pager
         viewPager.adapter = MainPagerAdapter(supportFragmentManager, Date())
         tabLayout.setupWithViewPager(viewPager)
+
+        // setup fab toolbar
+        fabToolbar.setFab(fab)
+        fab.onClick {
+            fabToolbar.expandFab()
+        }
+
+        // setup fab toolbar with view pager
+        viewPager.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener() {
+            override fun onPageSelected(position: Int) {
+                info { "SLIDE IN" }
+                fabToolbar.slideInFab()
+            }
+        })
     }
 }
+
