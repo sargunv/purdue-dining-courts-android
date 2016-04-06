4ef37474ddaffb4b09bb9fdb4f3b0de775207849
Sargun Vohra <sargunster@gmail.com>
2016-04-05 21:32:00 -0400

    finish fab toolbar

--- app/src/main/java/me/sargunvohra/android/diningcourts/main/MainActivity.kt
+++ app/src/main/java/me/sargunvohra/android/diningcourts/main/MainActivity.kt
@@ -1,14 +1,28 @@
 package me.sargunvohra.android.diningcourts.main
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewPager
+import android.support.v7.widget.LinearLayoutManager
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.karumi.rosie.renderer.RosieRenderer
+import com.pedrogomez.renderers.ListAdapteeCollection
+import com.pedrogomez.renderers.RVRendererAdapter
+import com.pedrogomez.renderers.Renderer
+import com.pedrogomez.renderers.RendererBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import me.sargunvohra.android.diningcourts.R
 import me.sargunvohra.android.diningcourts.base.BaseActivity
 import me.sargunvohra.android.diningcourts.config.dagger.MainModule
-import org.jetbrains.anko.info
-import org.jetbrains.anko.onClick
-import org.jetbrains.anko.support.v4.onPageChangeListener
+import me.sargunvohra.lib.ktunits.days
+import org.jetbrains.anko.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : BaseActivity() {
@@ -35,11 +49,7 @@ class MainActivity : BaseActivity() {
 
         // setup fab toolbar with view pager
         viewPager.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener() {
-            override fun onPageSelected(position: Int) {
-                info { "SLIDE IN" }
-                fabToolbar.slideInFab()
-            }
+            override fun onPageSelected(position: Int) = fabToolbar.slideInFab()
         })
     }
 }
-
