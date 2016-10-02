87749477c7c5a842451efca56db6ef8c51f957e6
Sargun Vohra <sargunster@gmail.com>
2016-10-02 17:39:20 -0400

    basic layout for main activity

--- app/src/main/java/me/sargunvohra/android/diningcourts/main/MainActivity.java
+++ app/src/main/java/me/sargunvohra/android/diningcourts/main/MainActivity.java
@@ -2,6 +2,7 @@ package me.sargunvohra.android.diningcourts.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import me.sargunvohra.android.diningcourts.R;
 
@@ -11,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 }
