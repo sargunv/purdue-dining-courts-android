0bc2a6dafb609c6488f134f415629d2444e6fc01
Sargun Vohra <sargunster@gmail.com>
2016-10-02 23:31:31 -0400

    top bar animation

--- app/src/main/java/me/sargunvohra/android/diningcourts/main/MainActivity.java
+++ app/src/main/java/me/sargunvohra/android/diningcourts/main/MainActivity.java
@@ -1,12 +1,21 @@
 package me.sargunvohra.android.diningcourts.main;
 
+import android.graphics.Color;
+import android.support.annotation.ColorRes;
+import android.support.annotation.IdRes;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
 import me.sargunvohra.android.diningcourts.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnTabSelectListener {
+
+    private ToolbarColorAnimator toolbarColorAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +24,29 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbarColorAnimator = new ToolbarColorAnimator(toolbar, Color.BLACK);
 
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(this);
+    }
 
+    @Override
+    public void onTabSelected(@IdRes int tabId) {
+        @ColorRes int color;
+        switch (tabId) {
+            case R.id.tab_dining:
+                color = R.color.primary_dining;
+                break;
+            case R.id.tab_restaurants:
+                color = R.color.primary_restaurants;
+                break;
+            case R.id.tab_settings:
+                color = R.color.primary_settings;
+                break;
+            default:
+                throw new IllegalStateException("Unhandled bottom bar tab!");
+        }
+        toolbarColorAnimator.animateToColor(ContextCompat.getColor(MainActivity.this, color));
     }
 }
+
---
+++ app/src/main/java/me/sargunvohra/android/diningcourts/main/ToolbarColorAnimator.java
@@ -0,0 +1,35 @@
+package me.sargunvohra.android.diningcourts.main;
+
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.support.annotation.ColorInt;
+import android.support.v7.widget.Toolbar;
+
+public class ToolbarColorAnimator {
+    private @ColorInt
+    int currentColor;
+    private Toolbar toolbar;
+
+    public ToolbarColorAnimator(Toolbar toolbar, @ColorInt int startingColor) {
+        this.toolbar = toolbar;
+        this.currentColor = startingColor;
+        applyColor(currentColor);
+    }
+
+    private void applyColor(@ColorInt int newColor) {
+        toolbar.setBackgroundColor(newColor);
+    }
+
+    public void animateToColor(@ColorInt int newColor) {
+        ValueAnimator anim = ValueAnimator.ofObject(new ArgbEvaluator(), currentColor, newColor);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int color = (Integer) animation.getAnimatedValue();
+                currentColor = color;
+                applyColor(color);
+            }
+        });
+        anim.start();
+    }
+}
